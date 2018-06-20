2a4b123c4314cbdbb3c13fe9f4c3deab4f557444
praginraj <pragin.martin@hotmail.com>
2018-06-20 13:28:15 +0100

    Added mongoose, code to receive data from and to mongoDB

--- app.js
+++ app.js
@@ -2,24 +2,37 @@ const express =  require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
  //const expressValidator = require('express-validatior');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/customers');
+const db = mongoose.connection;
 
 const app = express();
 
+let Schema = mongoose.Schema;
+
+//create a Schema
+let userSchema = new Schema({
+  first_name: String,
+  last_name: String,
+  email: String
+});
+
+let User = mongoose.model('User',userSchema);
 
 // Middlewares
-let myLogger = (req, res, next) => {
-  console.log('LOGGING..');
-  next();
-}
+// let myLogger = (req, res, next) => {
+//   console.log('LOGGING..');
+//   next();
+// }
 
-app.use(myLogger);
-
-let requestTime = (req, res, next) => {
-    req.requestTime = Date.now();
-    next();
-}
-
-app.use(requestTime);
+// // app.use(myLogger);
+//
+// let requestTime = (req, res, next) => {
+//     req.requestTime = Date.now();
+//     next();
+// }
+//
+// app.use(requestTime);
 
 //View Engine
 app.set('view engine', 'ejs');
@@ -32,41 +45,35 @@ app.use(bodyParser.urlencoded({extened: false}));
 //Set static path
 app.use(express.static(path.join(__dirname, 'public')))
 
-let users = [
-  {
-    id: 1,
-    first_name: 'Pragin',
-    last_name: 'Martin',
-    age: 33
-  },
-  {
-    id: 2,
-    first_name: 'Shenora',
-    last_name: 'Arul',
-    age: 30
-  },
-  {
-    id: 3,
-    first_name: 'sandra',
-    last_name: 'Kacka',
-    age: 33
-  }]
+let user = new User({
+  first_name: 'sandra',
+  last_name: 'Raj',
+  email: 'aaa@hotmail.com'
+
+});
 
 app.get('/', (req, res) => {
-  // let responseText = 'Hello World<br/>'
-  // responseText += '<small> Requested at : ' + req.requestTime + '</small>'
-  res.render('index',{
-    title: 'Customers',
-    users: users
+  db.on('error', console.error.bind(console, 'connection error'));
+    User.find({}, function(err, users){
+      if(err) throw err;
+      res.render('index',{
+       title: 'Customers',
+       users: users
+     });
   });
 });
 
 app.post('/users/add', (req, res) => {
-  let user = {
+  let newUser = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     email: req.body.email
   }
-  console.log(user);
-});
+
+  db.on('error', console.error.bind(console, 'connection error'));
+      User.create(newUser, function(err, result) {
+        if (err) console.log(err);
+        res.redirect('/');
+    });
+  });
 app.listen(3000, () => console.log('Server started at port 3000...'));
